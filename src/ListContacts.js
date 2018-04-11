83cadf6dc5e33346a84bca0c6746259837a63b5b
Nate Jensen <najens@gmail.com>
2018-04-11 15:46:18 -0600

    docs: Add state and remove contact function

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class ListContacts extends Component {
-    render() {
-
-        return(
-            <ol className='contact-list'>
-            
-            </ol>
-        )
-    }
-}
+const ListContacts = (props) => (
+    <ol className='contact-list'>
+        {props.contacts.map((contact) => (
+            <li key={contact.id} className='contact-list-item'>
+                <div
+                    className='contact-avatar'
+                    style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}
+                ></div>
+                <div className='contact-details'>
+                    <p>{contact.name}</p>
+                    <p>{contact.handle}</p>
+                </div>
+                <button
+                    onClick={() => props.onDeleteContact(contact)}
+                    className='contact-remove'></button>
+            </li>
+        ))}
+    </ol>
+);
 
 export default ListContacts;
